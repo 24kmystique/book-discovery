760d67c7794207a493abcf4561fb0b3617e159a7
24kmystique <itsliwens@gmail.com>
2023-03-06 15:28:09 +0800

    added ad-card

---
+++ src/components/ad-card/AdCard.js
@@ -0,0 +1,12 @@
+import "./AdCard.css";
+
+function AdCard() {
+  return (
+    <div className="ad-card__wrapper">
+      <img className="ad-card__img" src="https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/content/Deals_Store_sidebar.jpg" alt="Enoy great discounts at the deals store" />
+      <p className="ad-card__txt">Enjoy great discounts at the deals store</p>
+    </div>
+  )
+}
+
+export default AdCard;
