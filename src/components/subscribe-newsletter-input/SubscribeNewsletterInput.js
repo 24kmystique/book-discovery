aefcb87b15156e92aedf53c1b36cbb26411b9cf6
24kmystique <itsliwens@gmail.com>
2023-03-06 21:14:49 +0800

    added subscribe-newsletter div

---
+++ src/components/subscribe-newsletter-input/SubscribeNewsletterInput.js
@@ -0,0 +1,21 @@
+import "./SubscribeNewsletterInput.css";
+import "../button/Button.css";
+
+function SubscribeNewsletterInput() {
+  return (
+    <div className="sub-news-input__wrapper">
+      <div className="sub-news-input__img-txt-wrapper">
+        <img className="sub-news-input__img" src="https://d3ogvdx946i4sr.cloudfront.net/assets/v2.25.81/img/newsletter-icon.svg" alt="Learn about new offers and get more deals by joining our newsletter" />
+        <p className="sub-news-input__txt">Learn about new offers and get more deals by joining our newsletter</p>
+      </div>
+      <input className="sub-news-input__input" placeholder="Email address" />
+
+      <div className="sub-news-input__btn-wrapper">
+        <button className="btn__primary btn__primary--bolded">Sign up now</button>
+      </div>
+      
+    </div>
+  )
+}
+
+export default SubscribeNewsletterInput;
